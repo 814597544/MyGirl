8748442e0b9e6a1bc137dbfe5cd7e5af0a5b9dc8
814597544 <814597544@qq.com>
2015-05-05 17:36:38 +0800

    2.4

--- app/src/main/java/com/example/administrator/mygirl/NotepadActivity.java
+++ app/src/main/java/com/example/administrator/mygirl/NotepadActivity.java
@@ -53,7 +53,7 @@ public class NotepadActivity extends Activity implements  SwipeRefreshLayout.OnR
     DatabaseHelper databaseHelper;
     SQLiteDatabase sqLiteDatabase;
     LinearLayout notelayout;
-    private long exitTime = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -354,26 +354,5 @@ public class NotepadActivity extends Activity implements  SwipeRefreshLayout.OnR
         }
     };
 
-    /*--------按两次返回键退出程序--------*/
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN)
-        {
 
-            if((System.currentTimeMillis()-exitTime) > 2000)  //System.currentTimeMillis()无论何时调用，肯定大于2000
-            {
-                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
-                exitTime = System.currentTimeMillis();
-            }
-            else
-            {
-                finish();
-                System.exit(0);
-            }
-
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
 }
